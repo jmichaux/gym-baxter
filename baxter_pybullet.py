c1be18054885aa06cae6d828e9616f7aaef7d914
RIPL <jmichaux@ttic.edu>
2018-07-09 16:20:26 -0500

    skeleton for baxter pybullet interface

---
+++ baxter_pybullet.py
@@ -0,0 +1,15 @@
+import copy
+import sys
+
+import pybullet as p
+
+
+class BaxterPyBullet(object):
+    def __init__(self, control=None, limbs=None):
+        pass
+
+    def reset(self):
+        pass
+
+    def get_joint_angles(self):
+        pass
