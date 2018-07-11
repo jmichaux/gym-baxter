15afad51ff8c80351a2aeb43c65b503141a8c4a8
RIPL <jmichaux@ttic.edu>
2018-07-11 14:46:36 -0500

    small changes to baxter_env skeleton

--- ripl_control/baxter_env.py
+++ ripl_control/baxter_env.py
@@ -1,33 +1,43 @@
+import random
+import time
+
+import numpy as np
+import rospy
+
 import gym
 from gym import spaces
 from gym.utils import seeding
-import numpy as np
-import time
-import pybullet as p
-from . import baxter
-import random
-import pybullet_data
 
+from baxter_ros import BaxterRos
 
+class BaxterEnv(gym.Env):
+    metadata = {
+        'render.modes': ['human', 'rgb_array'],
+        'video.frames_per_second': 50}
 
+    def __init__(self, action_=1, isEnableSelfCollision=True, renders=False, isDiscrete=False, max_steps=1000):
+        # initialize ros node
+        rospy.init_node("reacher_env", anonymous=False)
 
-class BaxterEnv(gym.EnV):
-  metadata = {
-    'render.modes': ['human', 'rgb_array'],
-    'video.frames_per_second': 50
-  }
+        self._isDiscrete = isDiscrete
+        self._timestep = 1./240.
+        self.reset()
 
+    def _step(self, actions):
+        raise NotImplementedError
 
-  def __init__(self, action_=1, isEnableSelfCollision=True, renders=False, isDiscrete=False, max_steps=1000):
-    self._isDiscrete = isDiscrete
-    self._timestep = 1./240.
-    self.reset()
+    def _reset(self):
+        # initialilze baxter
+        self.baxter = Baxter()
+        pass
 
-  def _step(self, actions):
-    pass
+    def _render(self, mode='human', close=False):
+        pass
 
-  def _reset(self):
-    pass
+    @property
+    def action_space(self):
+        pass
 
-  def _render(self, mode='human', close=False):
-    pass
+    @property
+    def observation_space(self):
+        pass
