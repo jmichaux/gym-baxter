5dbc0af5c18e54e010f6fd16428be2655f0e609a
RIPL <jmichaux@ttic.edu>
2018-08-02 14:51:51 -0500

    changed imports

--- ripl_control/envs/__init__.py
+++ ripl_control/envs/__init__.py
@@ -1,5 +1,8 @@
-from ripl_control.envs.baxter import Baxter
-from ripl_control.envs.baxter_env import BaxterReacherEnv
+# from ripl_control.envs.baxter import *
+# from ripl_control.envs.baxter_envs.baxter_reacher_env import BaxterReacherEnv
+from baxter_envs import BaxterReacherEnv
+
+# from ripl_control.envs.baxter_env import BaxterReacherEnv
 # from ripl_control.envs.baxter_env import BaxterPickPlaceEnv
 # from ripl_control.envs.baxter_env import BaxterClearTableEnv
 #
@@ -7,3 +10,4 @@ from ripl_control.envs.baxter_env import BaxterReacherEnv
 # from ripl_control.envs.baxter_env import UR5ReacherEnv
 # from ripl_control.envs.baxter_env import UR5PickPlaceEnv
 # from ripl_control.envs.baxter_env import UR5ClearTableEnv
+# from ripl_control.envs.baxter import Baxter
