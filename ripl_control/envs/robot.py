22c4c7ef342f66c84be9e5cafd40af251826e82e
RIPL <jmichaux@ttic.edu>
2018-08-09 12:34:37 -0500

    more stuff that needs to be moved

---
+++ ripl_control/envs/robot.py
@@ -0,0 +1,15 @@
+class Robot(object):
+    def __init__(self):
+        pass
+
+    def reset(self):
+        raise NotImplementedError
+
+    def apply_action(self):
+        raise NotImplementedError
+
+    def get_state(self):
+        raise NotImplementedError
+
+    def apply_action(self):
+        raise NotImplementedError
