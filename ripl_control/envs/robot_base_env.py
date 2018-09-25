4833f3bee423faff66e2e0b0519c7243dfcabd11
RIPL <jmichaux@ttic.edu>
2018-09-25 15:04:25 -0500

    updating envs to follow gym.GoalEnv convention

---
+++ ripl_control/envs/robot_base_env.py
@@ -0,0 +1,83 @@
+"""
+Adopted from OpenAI gym
+https://github.com/openai/gym/blob/master/gym/envs/robotics/robot_env.py
+"""
+import numpy as np
+
+import gym
+from gym import error, spaces
+from gym.utils import seeding
+
+class RobotBaseEnv(gym.GoalEnv):
+    def __init__(self, sim, n_actions, n_substeps):
+        self.sim = sim
+
+        self.seed()
+        self.goal = self._sample_goal()
+        obs = self._get_obs()
+        self.action_space = 0
+        self.observation_space = 0
+
+    def seed(self, seed=None):
+        self.np_random, seed = seeding.np_random(seed)
+        return [seed]
+
+    def step(self, action):
+        action = np.clip(action, self.action_space.low, self.action_space.high)
+        self._apply_action(action)
+        obs = self._get_obs()
+        done = False
+        info = {
+            'is_success': self._is_succes(obs['achieved_goal'], self.goal)
+            }
+        reward = self.compute_reward(obs['achieved_goal'], self.goal, info)
+        return
+
+    def reset(self):
+        """Resets the environment
+        """
+        raise NotImplementedError
+
+    def _get_obs(self):
+        """Returns the observation.
+        """
+        raise NotImplementedError()
+
+    def _apply_action(self, action):
+        """Applies the given action to the simulation.
+        """
+        raise NotImplementedError()
+
+    def _is_success(self, achieved_goal, desired_goal):
+        """Indicates whether or not the achieved goal successfully achieved the desired goal.
+        """
+        raise NotImplementedError()
+
+    def _sample_goal(self):
+        """Samples a new goal and returns it.
+        """
+        raise NotImplementedError()
+
+    def _env_setup(self, initial_qpos):
+        """Initial configuration of the environment. Can be used to configure initial state
+        and extract information from the simulation.
+        """
+        pass
+
+    def _viewer_setup(self):
+        """Initial configuration of the viewer. Can be used to set the camera position,
+        for example.
+        """
+        pass
+
+    def _render_callback(self):
+        """A custom callback that is called before rendering. Can be used
+        to implement custom visualizations.
+        """
+        pass
+
+    def _step_callback(self):
+        """A custom callback that is called after stepping the simulation. Can be used
+        to enforce additional constraints on the simulation state.
+        """
+        pass
