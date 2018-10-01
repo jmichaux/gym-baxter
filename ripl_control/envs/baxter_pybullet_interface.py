f365c5215a020c758272429144316550eefcd6a3
RIPL <jmichaux@ttic.edu>
2018-10-01 18:35:59 -0500

    many changes that i forgot to commit

--- ripl_control/envs/baxter_pybullet_interface.py
+++ ripl_control/envs/baxter_pybullet_interface.py
@@ -5,85 +5,60 @@ class Limb(object):
     """
     PyBullet interface class that mimics the Limb class from baxter_interface.limb.
     """
-    def __init__(self, robot_id, name):
-        self.robot_id = robot_id
-        self.name = name # right or left
-
-        if self.name == 'right':
-            self.ee_index = 26
-            self.ros_to_pb = {'right_s0' : 12,
-                              'right_s1' : 13,
-                              'right_e0' : 14,
-                              'right_e1' : 15,
-                              'right_w0' : 16,
-                              'right_w1' : 18,
-                              'right_w2' : 19}
-
-            self.pb_to_ros = {12 : 'right_s0',
-                              13 : 'right_s1',
-                              14 : 'right_e0',
-                              15 : 'right_e1',
-                              16 : 'right_w0',
-                              18 : 'right_w1',
-                              19 : 'right_w2'}
+    def __init__(self, baxter_id, name, config):
+        self.baxter_id = baxter_id
+        self.name = name
+        self.config = config
+        if self.name not in ['left', 'right']:
+            raise ValueError("Arg 'name' must be 'right' or 'left'.")
+        if self.name == 'left':
+            self._joint_names = ['left_s0', 'left_s1', 'left_e0', 'left_e1',
+                                'left_w0', 'left_w1', 'left_w2']
+            self.joints = self.joint_indices = [34, 35, 36, 37, 38, 40, 41]
+            self.ee = self.ee_index = 48
         else:
-            self.ee_index = 48
-            self.ros_to_pb = {'left_s0' : 12,
-                              'left_s1' : 13,
-                              'left_e0' : 14,
-                              'left_e1' : 15,
-                              'left_w0' : 16,
-                              'left_w1' : 18,
-                              'left_w2' : 19}
-
-            self.pb_to_ros = {34 : 'left_s0',
-                              35 : 'left_s1',
-                              36 : 'left_e0',
-                              37 : 'left_e1',
-                              38 : 'left_w0',
-                              40 : 'left_w1',
-                              41 : 'left_w2'}
-        self.joint_indices = self.pb_to_ros.keys()
+            self._joint_names = ['right_s0', 'right_s1', 'right_e0', 'right_e1',
+                                'right_w0', 'right_w1', 'right_w2']
+            self.joints = self.joint_indices = [12, 13, 14, 15, 16, 18, 19]
+            self.ee = self.ee_index = 26
+        # Real2Sim conversions
+        self._name_2_ind = dict(zip(self._joint_names, self.joint_indices))
+        self._ind_2_name = dict(zip(self.joint_indices, self._joint_names))
 
     def joint_names(self):
-        return self.pb_to_ros.values()
+        return self._joint_names
 
     def joint_angle(self, joint):
-        if joint not in self.ros_to_pb:
-            raise ValueError('Joint {} is not the name of a joint'.format(joint))
-        return p.getJointState(self.baxter_id, self.ros_to_pb[joint])[0]
+        return p.getJointState(self.baxter_id, self.joint_indices[joint])[0]
 
     def joint_angles(self):
-        joint_dict = dict()
-        for key in self.pb_to_ros.keys():
-            joint_dict[self.pb_to_ros[key]] = p.getJointState(self.baxter_id, key)[0]
-        return joint_dict
+        joints = []
+        for joint_index in self.joint_indices:
+            joints.append(p.getJointState(self.baxter_id, joint_index)[0])
+        return joints
 
     def joint_velocity(self, joint):
-        if joint not in self.ros_to_pb:
-            raise ValueError('Joint {} is not the name of a joint'.format(joint))
-        return p.getJointState(self.baxter_id, self.ros_to_pb[joint])[1]
+        return p.getJointState(self.baxter_id, self.joint_indices[joint])[1]
 
     def joint_velocities(self):
-        joint_dict = dict()
-        for key in self.pb_to_ros.keys():
-            joint_dict[self.pb_to_ros[key]] = p.getJointState(self.baxter_id, key)[1]
-        return joint_dict
+        joints = []
+        for joint_index in self.joint_indices:
+            joints.append(p.getJointState(self.baxter_id, joint_index)[1])
+        return joints
 
     def joint_effort(self, joint):
-        if joint not in self.ros_to_pb:
-            raise ValueError('Joint {} is not the name of a joint'.format(joint))
-        return p.getJointState(self.baxter_id, self.ros_to_pb[joint])[3]
+        return p.getJointState(self.baxter_id, self.joint_indices[joint])[3]
 
     def joint_efforts(self):
-        joint_dict = dict()
-        for key in self.pb_to_ros.keys():
-            joint_dict[self.pb_to_ros[key]] = p.getJointState(self.baxter_id, key)[3]
-        return joint_dict
+        joints = []
+        for joint_index in self.joint_indices:
+            joints.append(p.getJointState(self.baxter_id, joint_index)[3])
+        return joints
 
     def endpoint_pose(self):
-        pos, orn = p.getLinkState(self.robot_id, self._ee_index)
-        return {'orientation': list(orn), 'position': list(pos)}
+        pos, orn = p.getLinkState(self.baxter_id, self.ee_index)[:2]
+        return {'position': pos, 'orientation': orn}
+
 
     def endpoint_velocity(self):
         """
@@ -101,105 +76,94 @@ class Limb(object):
         # This might not  be necessary
         return
 
-    def set_joint_positions(self, joints_dict, control_type,
-                                max_force, max_vel, pos_gain, vel_gain):
+    def set_joint_positions(self, joints):
         """
         This is kinda silly, but it matches the syntax in baxter.py
         in order to execute control commands for both arms simultaneously.
         """
-        self.move_to_joint_positions(joints_dict, control_type,
-                                    max_force, max_vel, pos_gain, vel_gain)
+        self.move_to_joint_positions(joints)
         return
 
-    def set_joint_velocities(self, joints_dict, control_type,
-                                max_force, max_vel, pos_gain, vel_gain):
-        """
-        Args
-            joints (dict)
-        """
-        joints, velocities = joints_dict.keys(), joints_dict.value()
-        p.setJointMotorControlArray(bodyUniqueId=self.baxter_id,
-                                    jointIndices=joints,
-                                    controlMode=control_type,
-                                    # targetPositions=angles,
-                                    targetVelocity=velocities,
-                                    force=max_force,
-                                    maxVelocity=max_vel,
-                                    positionGain=pos_gain,
-                                    velocityGain=vel_gain)
-        return
-
-    def set_joint_torques(self, joints_dict, control_type,
-                                max_force, max_vel, pos_gain, vel_gain):
-        """
-        Args
-            joints (dict)
-        """
-        #TODO Fix this
-        joints, torques = joints_dict.keys(), joints_dict.value()
-        p.setJointMotorControlArray(bodyUniqueId=self.baxter_id,
-                                    jointIndices=joints,
-                                    controlMode=control_type,
-                                    # targetPositions=angles,
-                                    targetVelocity=0,
-                                    force=max_force,
-                                    maxVelocity=max_vel,
-                                    positionGain=pos_gain,
-                                    velocityGain=vel_gain)
-        return
+    # def set_joint_velocities(self,
+    #                          joints_velocities,
+    #                          target_positions=self.config.target_positions,
+    #                          max_force=self.config.max_force,
+    #                          max_velocity=self.config.max_velocity,
+    #                          position_gain=self.config.position_gain,
+    #                          velocity_gain=self.config.velocity_gain):
+    #
+    #     """
+    #     Args
+    #         joints (list)
+    #     """
+    #     joint_indices, velocities = joints_dict.keys(), joints_dict.value()
+    #     p.setJointMotorControlArray(bodyUniqueId=self.baxter_id,
+    #                                 jointIndices=self.joint_indices,
+    #                                 controlMode=p.VELOCITY_CONTROL,
+    #                                 targetPositions=target_positions,
+    #                                 targetVelocity=joint_velocities,
+    #                                 force=max_force,
+    #                                 maxVelocity=max_velocity,
+    #                                 positionGain=position_gain,
+    #                                 velocityGain=velocity_gain)
+    #     return
+    #
+    # def set_joint_torques(self,
+    #                       joint_torques,
+    #                       target_positions=self.config.target_positions,
+    #                       target_velocity=self.config.target_velocity,
+    #                       max_force=self.config.max_force,
+    #                       max_velocity=self.config.max_velocity,
+    #                       position_gain=self.config.position_gain,
+    #                       velocity_gain=self.config.velocity_gain):
+    #
+    #
+    #     """
+    #     Args
+    #         joints (list): List of joint position values
+    #     """
+    #     p.setJointMotorControlArray(bodyUniqueId=self.baxter_id,
+    #                                 jointIndices=self.joint_indices,
+    #                                 controlMode=p.TORQUE_CONTROL,
+    #                                 targetPositions=target_positions,
+    #                                 targetVelocity=target_velocity,
+    #                                 force=joint_torques,
+    #                                 maxVelocity=max_velocity,
+    #                                 positionGain=position_gain,
+    #                                 velocityGain=velocity_gain)
+    #     return
 
     def move_to_neutral(self):
-        initial_pose = [
-          # right arm
-          [12, 0.00],
-          [13, -0.55],
-          [14, 0.00],
-          [15, 0.75],
-          [16, 0.00],
-          [18, 1.26],
-          [19, 0.00],
-          # right gripper
-          [27, -0.0052],
-          [29, -0.0052],
-          # left arm
-          [34, 0.00],
-          [35, -0.55],
-          [36, 0.00],
-          [37, 0.75],
-          [38, 0.00],
-          [40, 1.26],
-          [41, 0.00],
-          # left gripper
-          [49, -0.0052],
-          [51, -0.0052]]
-
-        # set position of arms
+        initial_pose = self.config.initial_pose
         for joint_index, joint_val in initial_pose:
             p.resetJointState(self.baxter_id, joint_index, joint_val)
             p.setJointMotorControl2(baxter, joint_index, p.POSITION_CONTROL, joint_val, force=self.max_force)
-        return self.move_to_joint_positions(angles, timeout)
 
-    def move_to_joint_positions(self, joints_dict, control_type,
-                                max_force, max_vel, pos_gain, vel_gain):
+    def move_to_joint_positions(self, joint_angles):
+        joint_indices=self.joint_indices
+        target_velocity=self.config.target_velocity
+        max_force=self.config.max_force
+        max_velocity=self.config.max_velocity
+        position_gain=self.config.position_gain
+        velocity_gain=self.config.velocity_gain
+        joint_indices = [5, 12, 13, 14, 15, 16, 18, 19, 27, 29, 34, 35, 36, 37, 38, 40, 41, 49, 51]
         """
         Args
-            joints (dict)
+            joints (list): List of joint position values
         """
-        joints, angles = joints_dict.keys(), joints_dict.value()
-        p.setJointMotorControlArray(bodyUniqueId=self.baxter_id,
-                                    jointIndices=joints,
-                                    controlMode=control_type,
-                                    targetPositions=angles,
-                                    targetVelocity=0,
-                                    force=max_force,
-                                    maxVelocity=max_vel,
-                                    positionGain=pos_gain,
-                                    velocityGain=vel_gain)
+        p.setJointMotorControlArray(bodyIndex=self.baxter_id,
+                                    jointIndices=joint_indices,
+                                    controlMode=p.POSITION_CONTROL,
+                                    targetPositions=joint_angles)
+                                    # targetVelocities=target_velocity,
+                                    # forces=max_force,
+                                    # positionGains=position_gain,
+                                    # velocityGains=velocity_gain)
         return
 
 class Gripper(object):
-    def __init__(self, robot_id, gripper):
-        self.robot_id = robot_id
+    def __init__(self, baxter_id, gripper):
+        self.baxter_id = baxter_id
 
     def close(self):
         pass
