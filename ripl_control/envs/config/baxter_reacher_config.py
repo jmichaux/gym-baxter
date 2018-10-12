f641cd4b28b9afba7696bdfaf23df8913282c0fb
RIPL <jmichaux@ttic.edu>
2018-10-12 16:51:28 -0500

    added proper joint velocity limits

--- ripl_control/envs/config/baxter_reacher_config.py
+++ ripl_control/envs/config/baxter_reacher_config.py
@@ -36,8 +36,37 @@ class PyBulletConfig():
         pass
 
 class ROSConfig():
-    def __init__(self):
-        self.all_joint_ranges = {
+    def __init__(self,
+                 ee_ranges=None,
+                 joint_position_ranges=None,
+                 joint_velocity_ranges=None):
+
+        self.right_joint_names = ['right_s0','right_s1','right_e0','right_e1','right_w0','right_w1','right_w2']
+        self.left_joint_names = ['left_s0','left_s1','left_e0','left_e1','left_w0','left_w1','left_w2']
+        self.joint_names = {'right': self.right_joint_names,
+                            'left': self.left_joint_names,
+                            'both': self.left_joint_names + self.right_joint_names}
+
+        self.joint_position_ranges = {}
+        self.joint_position_ranges['left'] = {
+            'left_s0' : {'min': -1.7016, 'max': 1.7016},
+            'left_s1' : {'min': -2.147, 'max': 2.147},
+            'left_e0' : {'min': -3.0541, 'max': 3.0541 },
+            'left_e1' : {'min': -0.05, 'max': 2.618},
+            'left_w0' : {'min': -3.059, 'max': 3.059 },
+            'left_w1' : {'min': -1.5707, 'max': 2.094},
+            'left_w2' : {'min': -3.059, 'max': 3.059}}
+
+        self.joint_position_ranges['right'] = {
+            'right_s0' : {'min': -1.7016, 'max': 1.7016},
+            'right_s1' : {'min': -2.147, 'max': 2.147},
+            'right_e0' : {'min': -3.0541, 'max': 3.0541 },
+            'right_e1' : {'min': -0.05, 'max': 2.618},
+            'right_w0' : {'min': -3.059, 'max': 3.059 },
+            'right_w1' : {'min': -1.5707, 'max': 2.094},
+            'right_w2' : {'min': -3.059, 'max': 3.059}}
+
+        self.joint_position_ranges['both'] = {
             'left_s0' : {'min': -1.7016, 'max': 1.7016},
             'left_s1' : {'min': -2.147, 'max': 2.147},
             'left_e0' : {'min': -3.0541, 'max': 3.0541 },
@@ -47,30 +76,67 @@ class ROSConfig():
             'left_w2' : {'min': -3.059, 'max': 3.059},
             'right_s0' : {'min': -1.7016, 'max': 1.7016},
             'right_s1' : {'min': -2.147, 'max': 2.147},
-            'right_e0' : {'min': -3.0541, 'max': 3.0541},
+            'right_e0' : {'min': -3.0541, 'max': 3.0541 },
             'right_e1' : {'min': -0.05, 'max': 2.618},
             'right_w0' : {'min': -3.059, 'max': 3.059 },
             'right_w1' : {'min': -1.5707, 'max': 2.094},
-            'right_w2' : {'min': -3.059, 'max': 3.059 }}
+            'right_w2' : {'min': -3.059, 'max': 3.059}}
 
-        self.left_joint_ranges = {
-            'left_s0' : {'min': -1.7016, 'max': 1.7016},
-            'left_s1' : {'min': -2.147, 'max': 2.147},
-            'left_e0' : {'min': -3.0541, 'max': 3.0541 },
-            'left_e1' : {'min': -0.05, 'max': 2.618},
-            'left_w0' : {'min': -3.059, 'max': 3.059 },
-            'left_w1' : {'min': -1.5707, 'max': 2.094},
-            'left_w2' : {'min': -3.059, 'max': 3.059}}
+        self.initial_joint_positions = {
+            'left': [0.0, -0.55, 0.0, 0.75, 0.0, 1.26, 0.0],
+            'right': [0.0, -0.55, 0.0, 0.75, 0.0, 1.26, 0.0],
+            'both': [0.0, -0.55, 0.0, 0.75, 0.0, 1.26, 0.0] * 2
+        }
 
-        self.right_joint_ranges = {
-            'right_s0' : {'min': -1.7016, 'max': 1.7016},
-            'right_s1' : {'min': -2.147, 'max': 2.147},
-            'right_e0' : {'min': -3.0541, 'max': 3.0541},
-            'right_e1' : {'min': -0.05, 'max': 2.618},
-            'right_w0' : {'min': -3.059, 'max': 3.059 },
-            'right_w1' : {'min': -1.5707, 'max': 2.094},
-            'right_w2' : {'min': -3.059, 'max': 3.059 }}
-        self.initial_pose = None
+        # max speeds
+        #s0,s1,e0,e1 - 2
+        #w0,w1,w2 - 4
+        self.joint_velocity_ranges = {}
+        self.joint_velocity_ranges['left'] = {
+            'left_s0' : {'min': -2.0, 'max': 2.0},
+            'left_s1' : {'min': -2.0, 'max': 2.0},
+            'left_e0' : {'min': -2.0, 'max': 2.0 },
+            'left_e1' : {'min': -2.0, 'max': 2.0},
+            'left_w0' : {'min': -4.0, 'max': 4.0 },
+            'left_w1' : {'min': -4.0, 'max': 4.0},
+            'left_w2' : {'min': -4.0, 'max': 4.0}}
+
+        self.joint_velocity_ranges['right'] = {
+            'right_s0' : {'min': -2.0, 'max': 2.0},
+            'right_s1' : {'min': -2.0, 'max': 2.0},
+            'right_e0' : {'min': -2.0, 'max': 2.0},
+            'right_e1' : {'min': -2.0, 'max': 2.0},
+            'right_w0' : {'min': -4.0, 'max': 4.0},
+            'right_w1' : {'min': -4.0, 'max': 4.0},
+            'right_w2' : {'min': -4.0, 'max': 4.0}}
+
+        self.joint_velocity_ranges['both'] = {
+            'left_s0' : {'min': -2.0, 'max': 2.0},
+            'left_s1' : {'min': -2.0, 'max': 2.0},
+            'left_e0' : {'min': -2.0, 'max': 2.0 },
+            'left_e1' : {'min': -2.0, 'max': 2.0},
+            'left_w0' : {'min': -4.0, 'max': 4.0 },
+            'left_w1' : {'min': -4.0, 'max': 4.0},
+            'left_w2' : {'min': -4.0, 'max': 4.0},
+            'right_s0' : {'min': -2.0, 'max': 2.0},
+            'right_s1' : {'min': -2.0, 'max': 2.0},
+            'right_e0' : {'min': -2.0, 'max': 2.0},
+            'right_e1' : {'min': -2.0, 'max': 2.0},
+            'right_w0' : {'min': -4.0, 'max': 4.0},
+            'right_w1' : {'min': -4.0, 'max': 4.0},
+            'right_w2' : {'min': -4.0, 'max': 4.0}}
+
+        self.max_joint_speed = 0
+        self.max_joint_accel = 0
+
+    def set_ee_ranges(self):
+        pass
+
+    def set_joint_position_ranges(self):
+        pass
+
+    def set_joint_velocity_ranges(self):
+        pass
 
     def get_random_pose(self):
         pass
